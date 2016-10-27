1220061c1cdbe7160afc5932a40195cac01cd2d6
agarridogarcia <agarridogarcia@atanasoff30.danielcastelao.org>
2016-10-27 11:37:55 +0200

    Boletin 5_7

---
+++ src/boletin5_7/Boletin5_7.java
@@ -0,0 +1,16 @@
+
+package boletin5_7;
+
+import javax.swing.JOptionPane;
+
+
+public class Boletin5_7 {
+
+    public static void main(String[] args) {
+     Opcion opcion1=new Opcion();
+     opcion1.opcions();
+     
+        
+    }
+    
+}
---
+++ src/boletin5_7/Opcion.java
@@ -0,0 +1,52 @@
+
+package boletin5_7;
+
+import javax.swing.JOptionPane;
+
+
+public class Opcion {
+   
+    
+  
+    public Opcion (){      
+        
+    }
+    public void opcions (){
+        
+        System.out.println("cuadrado\n triangulo \n circulo" );   
+        String opcion=JOptionPane.showInputDialog("Elixe opción: ");
+        opcion = opcion.toLowerCase();
+        switch(opcion)  {
+            
+            case "cuadrado":
+                float lado=Float.parseFloat(JOptionPane.showInputDialog ("Introduzca o lado do cuadrado "));
+                System.out.println( "Area do cuadrado: " + (lado*lado));
+            break;
+            case "triangulo": 
+                float base=Float.parseFloat(JOptionPane.showInputDialog("Introduzca o lado do triángulo: "));
+                float altura=Float.parseFloat(JOptionPane.showInputDialog ("Introduzca a altura do triángulo: "));
+                System.out.println("Area do triangulo: " + (base*altura));
+                break;
+            case "circulo": 
+                float radio=Float.parseFloat(JOptionPane.showInputDialog("Introduzca o radio do círculo: "));
+                System.out.println (Math.PI*Math.pow(radio, 2));
+                break;
+                default: System.out.println("Fora de rango");
+                
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        }     
+      
+    }
+    public void elixirOpcion(){
+        
+    }
+    
+}
